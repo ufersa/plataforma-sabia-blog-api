84e164f96e1a96cceae36a0f49e3e6ecfa67e293
Luiz Felicio <unifelicio@gmail.com>
2020-11-28 16:43:14 -0300

    refactor: adjust image alt text

--- admin/src/containers/AuthPage/components/Logo/index.js
+++ admin/src/containers/AuthPage/components/Logo/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import LogoSabiaPlatform from "../../../../assets/images/logo-sabia.svg";
 import Img from "./Img";
 
-const Logo = () => <Img src={LogoSabiaPlatform} alt="wongames-logo" />;
+const Logo = () => <Img src={LogoSabiaPlatform} alt="Logomarca da Plataforma Sabia" />;
 
 export default Logo;
