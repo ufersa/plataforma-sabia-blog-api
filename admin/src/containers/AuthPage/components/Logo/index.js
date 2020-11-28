064026fb540f16621c5a09087850cb160f550b2b
Luiz Felicio <unifelicio@gmail.com>
2020-11-28 16:13:51 -0300

    feat: add sabia logo to login page

---
+++ admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import LogoSabiaPlatform from "../../../../assets/images/logo-sabia.svg";
+import Img from "./Img";
+
+const Logo = () => <Img src={LogoSabiaPlatform} alt="wongames-logo" />;
+
+export default Logo;
