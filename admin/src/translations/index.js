3efb50ce67299e817420f5462f0aaf9b766d06bd
Luiz Felicio <unifelicio@gmail.com>
2020-11-28 17:35:20 -0300

    feat: add available locales

---
+++ admin/src/translations/index.js
@@ -0,0 +1,14 @@
+import ptBR from './pt-BR.json';
+import en from './en.json';
+
+const trads = {
+  en,
+  'pt-BR': ptBR,
+};
+
+export const languageNativeNames = {
+  'pt-BR': 'Português (Brasil)',
+  en: 'English'
+};
+
+export default trads;
