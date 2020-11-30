2322041c9e1a29192f6c12c7431f7418312d176f
Luiz Felicio <unifelicio@gmail.com>
2020-11-30 11:54:06 -0300

    feat: automatic post slugs

--- api/post/models/post.js
+++ api/post/models/post.js
@@ -1,8 +1,19 @@
-'use strict';
+const slugify = require('slugify');
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+module.exports = {
+  /**
+   * Triggered before user creation.
+   */
+  lifecycles: {
+    async beforeCreate(data) {
+      if (data.title) {
+        data.slug = slugify(data.title, { lower: true });
+      }
+    },
+    async beforeUpdate(params, data) {
+      if (data.title) {
+        data.slug = slugify(data.title, { lower: true });
+      }
+    },
+  },
+};
