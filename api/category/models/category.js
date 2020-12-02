d040776a2f9bd3facacd14caaecb4e688cd2edfc
Luiz Felicio <unifelicio@gmail.com>
2020-12-02 17:44:18 -0300

    Add slug creation to categories (#4)

--- api/category/models/category.js
+++ api/category/models/category.js
@@ -1,8 +1,19 @@
-'use strict';
+const slugify = require('slugify');
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+module.exports = {
+  /**
+   * Triggered before category creation.
+   */
+  lifecycles: {
+    async beforeCreate(data) {
+      if (data.name) {
+        data.slug = slugify(data.name, { lower: true });
+      }
+    },
+    async beforeUpdate(params, data) {
+      if (data.name) {
+        data.slug = slugify(data.name, { lower: true });
+      }
+    },
+  },
+};
