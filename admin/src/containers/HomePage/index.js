c96b40def6eff058344a701103b6d73aea2d921a
Luiz Felicio <unifelicio@gmail.com>
2020-12-02 11:07:18 -0300

    Add welcome message (#1)

--- admin/src/containers/HomePage/index.js
+++ admin/src/containers/HomePage/index.js
@@ -8,29 +8,7 @@ import React, { memo } from "react";
 import { FormattedMessage } from "react-intl";
 import PageTitle from "../../components/PageTitle";
 
-import {
-  ALink,
-  Block,
-  Container,
-  LinkWrapper,
-  P,
-  Separator,
-} from "./components";
-import SocialLink from "./SocialLink";
-
-const FIRST_BLOCK_LINKS = [
-  {
-    link:
-      "https://strapi.io/documentation/v3.x/getting-started/quick-start.html#_4-create-a-category-content-type",
-    contentId: "app.components.BlockLink.documentation.content",
-    titleId: "app.components.BlockLink.documentation",
-  },
-  {
-    link: "https://github.com/strapi/foodadvisor",
-    contentId: "app.components.BlockLink.code.content",
-    titleId: "app.components.BlockLink.code",
-  },
-];
+import { Block, Container, P } from "./components";
 
 const HomePage = () => {
   return (
@@ -44,30 +22,8 @@ const HomePage = () => {
             <Block>
               <h2 id="mainHeader">Bem vindo ao blog da Plataforma Sabiá!</h2>
               <P>
-                Mensagem
+                Fique por dentro dos voos da Sabiá!
               </P>
-              <Separator style={{ marginTop: 37, marginBottom: 36 }} />
-              <div style={{ display: "flex", justifyContent: "space-between" }}>
-                {FIRST_BLOCK_LINKS.map((data, index) => {
-                  const type = index === 0 ? "doc" : "code";
-
-                  return (
-                    <LinkWrapper
-                      href={data.link}
-                      target="_blank"
-                      key={data.link}
-                      type={type}
-                    >
-                      <FormattedMessage id={data.titleId}>
-                        {(title) => <p className="bold">{title}</p>}
-                      </FormattedMessage>
-                      <FormattedMessage id={data.contentId}>
-                        {(content) => <p>{content}</p>}
-                      </FormattedMessage>
-                    </LinkWrapper>
-                  );
-                })}
-              </div>
             </Block>
           </div>
         </div>
