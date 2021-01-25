277e3ccc3337a7fcb995aa7899bb9b9dbca2e4f0
Luiz Felicio? <unifelicio@gmail.com>
2021-01-25 10:55:46 -0300

    feat: add email strapi provider

--- config/plugins.js
+++ config/plugins.js
@@ -10,4 +10,19 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    provider: env('EMAIL_PROVIDER'),
+    providerOptions: {
+      host: env('EMAIL_SMTP_HOST'),
+      port: env('EMAIL_SMTP_PORT'),
+      auth: {
+        user: env('EMAIL_SMTP_USER'),
+        pass: env('EMAIL_SMTP_PASS'),
+      },
+    },
+    settings: {
+      defaultFrom: env('EMAIL_ADDRESS_FROM'),
+      defaultReplyTo: env('EMAIL_ADDRESS_REPLY'),
+    },
+  },
 });
